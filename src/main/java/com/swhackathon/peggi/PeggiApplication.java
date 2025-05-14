256a965a11255dc5aec0592dae628969d8638e2c
gioJioh <sjgio0515@naver.com>
2025-05-14 17:07:42 +0900

    setting

---
+++ src/main/java/com/swhackathon/peggi/PeggiApplication.java
@@ -0,0 +1,13 @@
+package com.swhackathon.peggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swhackathon/peggi/PeggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swhackathon.peggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
