faf7677eab8dde2e09920659a37b8432d1cda353
namgoong <cookie_31@naver.com>
2025-05-17 22:45:32 +0900

    refactor: cors setting allowed origins

--- src/main/java/com/swhackathon/peggi/config/SecurityConfig.java
+++ src/main/java/com/swhackathon/peggi/config/SecurityConfig.java
@@ -10,7 +10,7 @@ public class SecurityConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ahnai1.suwon.ac.kr","https://suwonai.store")
+                .allowedOrigins("http://ahnai1.suwon.ac.kr","http://suwonai.store")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
