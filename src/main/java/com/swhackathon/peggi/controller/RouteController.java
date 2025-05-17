eb01fe9cff2ab9d961957cc656c183c5984beb44
gioJioh <sjgio0515@naver.com>
2025-05-17 14:25:05 +0900

    refactor: llm 채팅 스트림으로 변경

--- src/main/java/com/swhackathon/peggi/controller/RouteController.java
+++ src/main/java/com/swhackathon/peggi/controller/RouteController.java
@@ -1,14 +1,14 @@
 package com.swhackathon.peggi.controller;
 
 import com.swhackathon.peggi.dto.ChatToLlmRequest;
-import com.swhackathon.peggi.dto.ChatToLlmResponse;
 import com.swhackathon.peggi.service.RouteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RouteController {
     private final RouteService routeService;
 
-    @PostMapping()
-    public ResponseEntity<ChatToLlmResponse> chatToLlm(@RequestBody ChatToLlmRequest requestBody) {
-        ResponseEntity<ChatToLlmResponse> llmResponse = routeService.chatToLlm(requestBody);
-        return ResponseEntity.status(llmResponse.getStatusCode()).body(llmResponse.getBody());
+    @PostMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<String> chatStream(@RequestBody ChatToLlmRequest request) {
+        return routeService.streamFromLlm(request);
     }
 }
