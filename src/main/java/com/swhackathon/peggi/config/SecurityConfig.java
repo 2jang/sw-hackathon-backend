e17200a09a8c3de0a24c41e9ac7ef45a7abb3d6a
2jang <jakeecc@naver.com>
2025-06-03 11:22:29 +0900

    로컬 호스트 CORS 추가

--- src/main/java/com/swhackathon/peggi/config/SecurityConfig.java
+++ src/main/java/com/swhackathon/peggi/config/SecurityConfig.java
@@ -10,7 +10,11 @@ public class SecurityConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ahnai1.suwon.ac.kr","http://suwonai.store.s3-website-us-east-1.amazonaws.com")
+                .allowedOrigins(
+                        "http://localhost:5173", // 로컬 개발용 프론트엔드 주소 추가
+                        "http://ahnai1.suwon.ac.kr",
+                        "http://suwonai.store.s3-website-us-east-1.amazonaws.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
