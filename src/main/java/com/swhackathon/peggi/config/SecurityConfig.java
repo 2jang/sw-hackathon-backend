5bbd67bb4f22e3c6ae783c9838aed858b64aa7d6
2jang <jakeecc@naver.com>
2025-06-08 18:29:39 +0900

    remove cors why? I will use nginx

--- src/main/java/com/swhackathon/peggi/config/SecurityConfig.java
+++ src/main/java/com/swhackathon/peggi/config/SecurityConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer{
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173"
-                )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //             .allowedOrigins(
+    //                     "http://localhost:5173"
+    //             )
+    //             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+    //             .allowedHeaders("*")
+    //             .allowCredentials(true);
+    // }
 }
